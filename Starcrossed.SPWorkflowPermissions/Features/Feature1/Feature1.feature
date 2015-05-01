e1dabe7f6f867294d35562706f98209cdca24dff
Paul Choquette <pchoquette@gmail.com>
2015-05-01 08:26:26 -0400

    Initial commit

---
+++ Starcrossed.SPWorkflowPermissions/Features/Feature1/Feature1.feature
@@ -0,0 +1,6 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="0e1cfac2-7b63-48d9-8431-e03604d51bd7" featureId="0e1cfac2-7b63-48d9-8431-e03604d51bd7" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="Starcrossed.SPWorkflowPermissions.Sandbox Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="4d17b52d-bd0c-4c74-a04a-a7364c043540" />
+  </projectItems>
+</feature>
---
+++ Starcrossed.SPWorkflowPermissions/Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SharePoint Workflow Permissions")]
+[assembly: AssemblyDescription("A custom set of workflow actions for SharePoint 2013 that manipulate permissions.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Paul Choquette")]
+[assembly: AssemblyProduct("SharePoint Workflow Permissions")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("79E84497-48A9-451A-9C12-2DB14B589AFB")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: System.Windows.Markup.XmlnsDefinition("wf://workflow.windows.net/$Current/$Activities", "SPAdminMenu2013Sandbox")]
+[assembly: System.Windows.Markup.XmlnsDefinition("wf://workflow.windows.net/$Current/$Activities", "Starcrossed.SPWorkflowPermissions")]
